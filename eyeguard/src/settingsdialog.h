0cb337dc6d49da9b28c7369bac38839c24490f71
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2019-02-26 21:50:03 +0300

    Update settingsdialog.h
    Use nullptr keyword for a null pointer.

--- eyeguard/src/settingsdialog.h
+++ eyeguard/src/settingsdialog.h
@@ -14,7 +14,7 @@ class SettingsDialog: public QDialog {
     Q_OBJECT
 
 public:
-    SettingsDialog (QWidget * parent = 0);
+    SettingsDialog (QWidget * parent = nullptr);
     ~SettingsDialog ();
 
 public:
